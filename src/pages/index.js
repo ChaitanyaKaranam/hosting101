fc34f7660dd231c75f30766725bd8832efd8c934
chaitanyakaranam <karanam.krishna.25@gmail.com>
2020-09-26 12:16:35 +0530

    Completed hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import CloudSVG from "../images/cloud-hosting.svg"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -9,8 +10,30 @@ import "../styles/main.scss"
 const IndexPage = () => (
   <Layout>
     <SEO title="Hosting101" />
-    <Typography/>
+    <Hero />
+    <Typography />
   </Layout>
+)
+
+const Hero = () => (
+  <div className="heroSection">
+    <div className="layout">
+      <div className="hero">
+        <div className="hero__content">
+          <h1 className="showcase">
+            Move beyond localhost
+          </h1>
+          <p>Check out in-depth video guides on how to host your next application</p>
+          <div className="hero__cta">
+            <button>Explore Guides</button>
+          </div>
+        </div>
+        <div className="hero__image">
+          <img alt="Hosting101" src={CloudSVG}/>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 export default IndexPage
